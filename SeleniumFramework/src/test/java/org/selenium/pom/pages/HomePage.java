33ff4f431c2747fc752e1abec96e66db3e0adc51
Moulali Syed <syedmoulali1997@gmail.com>
2022-07-18 17:04:57 +0530

    SeleniumTestNGFramework

---
+++ SeleniumFramework/src/test/java/org/selenium/pom/pages/HomePage.java
@@ -0,0 +1,21 @@
+package org.selenium.pom.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.selenium.pom.base.BasePage;
+
+public class HomePage extends BasePage {
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+
+	private final By storemenuLink = By.cssSelector("#menu-item-1227 > a");
+	
+	
+
+	public StorePage navigateToStoreUsingMenu() {
+		driver.findElement(storemenuLink).click();
+		return new StorePage(driver);
+	}
+}
